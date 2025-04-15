7022804f4fc34b6a3bf06412a3258c8f1c93d409
venturions <alex.eduardo10@hotmail.com>
2025-04-15 17:08:37 -0300

    Primeiro commit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Alex da Costa',
-    email: 'alex.eduardo10@hotmail.com',
-  },
-})
