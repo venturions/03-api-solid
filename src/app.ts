770ad5e3aed360b97d981711f753826e8cbc873f
venturions <alex.eduardo10@hotmail.com>
2025-04-15 20:34:09 -0300

    Relacionamentos entre tabelas

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
 import fastify from 'fastify'
-import { PrismaClient } from 'generated/prisma'
-
-const prisma = new PrismaClient()
 
 export const app = fastify()
-
-prisma.user.create({
-  data: {
-    name: 'Alex da Costa',
-    email: 'alex.eduardo10@hotmail.com',
-  },
-})
