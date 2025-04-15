e2cae9a449f49687c676df93e7eb16dd0cafb99a
venturions <alex.eduardo10@hotmail.com>
2025-04-15 18:07:13 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma'
+
+const prisma = new PrismaClient()
 
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'Alex da Costa',
+    email: 'alex.eduardo10@hotmail.com',
+  },
+})
