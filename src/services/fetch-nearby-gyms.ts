978e328c08ea628a0c3cd879334591367d47a293
venturions <alex.eduardo10@hotmail.com>
2025-04-20 17:25:23 -0300

    Serviço de academias próximas

---
+++ src/services/fetch-nearby-gyms.ts
@@ -0,0 +1,26 @@
+import { Gym } from 'generated/prisma'
+import { GymsRepository } from 'src/repositories/gyms-repository'
+
+interface FetchNearbyGymsServiceRequest {
+  userLatitude: number
+  userLongitude: number
+}
+
+interface FetchNearbyGymsServiceResponse {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGymsService {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    userLatitude,
+    userLongitude,
+  }: FetchNearbyGymsServiceRequest): Promise<FetchNearbyGymsServiceResponse> {
+    const gyms = await this.gymsRepository.findManyNearby({
+      latitude: userLatitude,
+      longitude: userLongitude,
+    })
+    return { gyms }
+  }
+}
