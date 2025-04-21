f1ad13671e37e209443d1e8fb2c0a9d9fd566f98
venturions <alex.eduardo10@hotmail.com>
2025-04-21 12:22:40 -0300

    Teste E2E da autenticação

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from 'src/app'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
