8ed4c6f095141943b0edeaaf843f60d50eccfa14
venturions <alex.eduardo10@hotmail.com>
2025-04-20 21:16:52 -0300

    Controller de perfil (middleware verificação jwt)

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,19 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserProfileService } from 'src/services/factories/make-get-user-profile-service'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileService()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
