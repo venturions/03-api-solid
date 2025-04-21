2b5d5fe6f14ff4d2942faf2fdc586e4301f6f05f
venturions <alex.eduardo10@hotmail.com>
2025-04-21 12:29:34 -0300

    Teste E2E do perfil

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -11,7 +11,7 @@ describe('Authenticate (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
+  it('should be able to authenticate', async () => {
     await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@example.com',
